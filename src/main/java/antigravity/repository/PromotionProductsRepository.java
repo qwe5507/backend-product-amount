ca0c15603594c78a87357ddd6ef1d89a8d4d5867
qwe5507 <apvmffkdls@gmail.com>
2023-06-05 18:12:12 +0900

    feat : 쿼리메소드 -> jpql fetch join으로 변경

--- src/main/java/antigravity/repository/PromotionProductsRepository.java
+++ src/main/java/antigravity/repository/PromotionProductsRepository.java
@@ -1,7 +1,15 @@
 package antigravity.repository;
 
+import antigravity.domain.entity.Product;
 import antigravity.domain.entity.PromotionProducts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PromotionProductsRepository extends JpaRepository<PromotionProducts, Integer> {
+
+    @Query("SELECT ps FROM PromotionProducts ps JOIN FETCH ps.promotion p WHERE ps.product = :product AND p.id IN :ids")
+    List<PromotionProducts> findByProductAndPromotionIds(@Param("product") Product product, @Param("ids") List<Integer> ids);
 }
